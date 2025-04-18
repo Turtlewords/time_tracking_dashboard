e85d943bca4997ee62e9a3fabe0f14a86cc42c7a
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-04-18 18:59:24 -0500

    finish js

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-const workCurrent = document.querySelector("work-current");
-const workPrev = document.querySelector("work-prev");
-const playCurrent = document.querySelector("play-current");
-const playPrev = document.querySelector("play-prev");
-const studyCurrent = document.querySelector("study-current");
-const studyPrev = document.querySelector("study-prev");
-const exerciseCurrent = document.querySelector("exercise-current");
-const exercisePrev = document.querySelector("exercise-prev");
-const socialCurrent = document.querySelector("social-current");
-const socialPrev = document.querySelector("social-prev");
-const selfCurrent = document.querySelector("self-care-current");
-const selfPrev = document.querySelector("self-care-prev");
+
 const containers = document.querySelectorAll(".container");
 
 
-const daily = document.querySelector("daily");
-const weekly = document.querySelector("weekly");
-const monthly = document.querySelector("monthly");
+const daily = document.querySelector("#daily");
+const weekly = document.querySelector("#weekly");
+const monthly = document.querySelector("#monthly");
 
-function initialize() {
+daily.addEventListener("click", displayDaily);
+
+weekly.addEventListener("click", displayWeekly);
+
+monthly.addEventListener("click", displayMonthly);
+
+function displayDaily() {
 
     fetch("data.json").then((response) => {
         if(!response.ok) {
@@ -30,9 +25,59 @@ function initialize() {
 
             const current = container.querySelector(".current");
             const prev = container.querySelector(".prev");
+            const title = container.querySelector(".title");
 
             current.textContent = data[index].timeframes.daily.current + " hrs";
             prev.textContent = data[index].timeframes.daily.previous + " hrs";
+            title.textContent = data[index].title
+            index++;
+        }) 
+    })
+
+}
+
+function displayWeekly() {
+
+    fetch("data.json").then((response) => {
+        if(!response.ok) {
+            throw new Error("Could not retrieve data.")
+        }
+        return response.json();
+    }).then(data => {
+        let index = 0;
+        containers.forEach((container) => {
+
+            const current = container.querySelector(".current");
+            const prev = container.querySelector(".prev");
+            const title = container.querySelector(".title");
+
+            current.textContent = data[index].timeframes.weekly.current + " hrs";
+            prev.textContent = data[index].timeframes.weekly.previous + " hrs";
+            title.textContent = data[index].title
+            index++;
+        }) 
+    })
+
+}
+
+function displayMonthly() {
+
+    fetch("data.json").then((response) => {
+        if(!response.ok) {
+            throw new Error("Could not retrieve data.")
+        }
+        return response.json();
+    }).then(data => {
+        let index = 0;
+        containers.forEach((container) => {
+
+            const current = container.querySelector(".current");
+            const prev = container.querySelector(".prev");
+            const title = container.querySelector(".title");
+
+            current.textContent = data[index].timeframes.monthly.current + " hrs";
+            prev.textContent = data[index].timeframes.monthly.previous + " hrs";
+            title.textContent = data[index].title
             index++;
         }) 
     })
@@ -41,6 +86,4 @@ function initialize() {
 
 
 
-
-initialize();
-console.log(containers)
+displayDaily();
