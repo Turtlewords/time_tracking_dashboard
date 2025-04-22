303b14c8459ceee1751fcb6dac107ff9d3b4d066
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-04-22 14:31:03 -0500

    refactor

--- index.js
+++ index.js
@@ -1,107 +1,105 @@
-
-const containers = document.querySelectorAll(".container");
-
-
+const cardContainer = document.querySelector("#card-container");
 const daily = document.querySelector("#daily");
 const weekly = document.querySelector("#weekly");
 const monthly = document.querySelector("#monthly");
 
-daily.addEventListener("click", () => {
+
+daily.addEventListener("click", displayDaily);
+weekly.addEventListener("click", displayWeekly);
+monthly.addEventListener("click", displayMonthly);
+
+const fetchData = async () => {
+    try {
+        const response = await fetch("data.json");
+        if (!response.ok) {
+            throw new Error("Could not retrieve data")
+        }
+        const data = await response.json();
+        return data;
+    } catch(error) {
+        alert(error.message);
+    }
+}
+
+async function displayDaily() {
+    let data = await fetchData();
+    let element = "";
+
+    console.log(data);
+
+    data.forEach((activity) => {
+        element += 
+        `<section class="card" id="${activity.title.toLowerCase().replace(" ", "-")}">
+        <div class="content">
+          <div class="content-top">
+            <h2 class="activity-title">${activity.title}</h2>
+            <img src="images/icon-ellipsis.svg" alt="ellipsis">
+          </div>
+          <div class="content-bottom">
+            <p class="current">${activity.timeframes.daily.current}hrs</p>
+            <p class="prev">Yesterday - ${activity.timeframes.daily.previous}hrs</p>
+          </div>
+        </div>
+      </section>`
+    })
+    cardContainer.innerHTML = element;
     daily.classList.add("white");
     weekly.classList.remove("white");
     monthly.classList.remove("white")
-    displayDaily();
-});
+}
 
+async function displayWeekly() {
+    let data = await fetchData();
+    let element = "";
 
-weekly.addEventListener("click", () => {
+    console.log(data);
+
+    data.forEach((activity) => {
+        element += 
+        `<section class="card" id="${activity.title.toLowerCase().replace(" ", "-")}">
+        <div class="content">
+          <div class="content-top">
+            <h2 class="activity-title">${activity.title}</h2>
+            <img src="images/icon-ellipsis.svg" alt="ellipsis">
+          </div>
+          <div class="content-bottom">
+            <p class="current">${activity.timeframes.weekly.current}hrs</p>
+            <p class="prev">Last week - ${activity.timeframes.weekly.previous}hrs</p>
+          </div>
+        </div>
+      </section>`
+    })
+    cardContainer.innerHTML = element;
     daily.classList.remove("white");
     weekly.classList.add("white");
     monthly.classList.remove("white")
-    displayWeekly();
-});
+}
 
+async function displayMonthly() {
+    let data = await fetchData();
+    let element = "";
 
-monthly.addEventListener("click", () => {
+    console.log(data);
+
+    data.forEach((activity) => {
+        element += 
+        `<section class="card" id="${activity.title.toLowerCase().replace(" ", "-")}">
+        <div class="content">
+          <div class="content-top">
+            <h2 class="activity-title">${activity.title}</h2>
+            <img src="images/icon-ellipsis.svg" alt="ellipsis">
+          </div>
+          <div class="content-bottom">
+            <p class="current">${activity.timeframes.monthly.current}hrs</p>
+            <p class="prev">Last month - ${activity.timeframes.monthly.previous}hrs</p>
+          </div>
+        </div>
+      </section>`
+    })
+    cardContainer.innerHTML = element;
     daily.classList.remove("white");
     weekly.classList.remove("white");
     monthly.classList.add("white")
-    displayMonthly();
-});
-
-
-function displayDaily() {
-
-    fetch("data.json").then((response) => {
-        if(!response.ok) {
-            throw new Error("Could not retrieve data.")
-        }
-        return response.json();
-    }).then(data => {
-        let index = 0;
-        containers.forEach((container) => {
-
-            const current = container.querySelector(".current");
-            const prev = container.querySelector(".prev");
-            const title = container.querySelector(".title");
-
-            current.textContent = data[index].timeframes.daily.current + "hrs";
-            prev.textContent = "Yesterday - " + data[index].timeframes.daily.previous + "hrs";
-            title.textContent = data[index].title
-            index++;
-        }) 
-    })
-
 }
-
-function displayWeekly() {
-
-    fetch("data.json").then((response) => {
-        if(!response.ok) {
-            throw new Error("Could not retrieve data.")
-        }
-        return response.json();
-    }).then(data => {
-        let index = 0;
-        containers.forEach((container) => {
-
-            const current = container.querySelector(".current");
-            const prev = container.querySelector(".prev");
-            const title = container.querySelector(".title");
-
-            current.textContent = data[index].timeframes.weekly.current + "hrs";
-            prev.textContent = "Last week - " + data[index].timeframes.weekly.previous + "hrs";
-            title.textContent = data[index].title
-            index++;
-        }) 
-    })
-
-}
-
-function displayMonthly() {
-
-    fetch("data.json").then((response) => {
-        if(!response.ok) {
-            throw new Error("Could not retrieve data.")
-        }
-        return response.json();
-    }).then(data => {
-        let index = 0;
-        containers.forEach((container) => {
-
-            const current = container.querySelector(".current");
-            const prev = container.querySelector(".prev");
-            const title = container.querySelector(".title");
-
-            current.textContent = data[index].timeframes.monthly.current + "hrs";
-            prev.textContent = "Last month - " + data[index].timeframes.monthly.previous + "hrs";
-            title.textContent = data[index].title
-            index++;
-        }) 
-    })
-
-}
-
-
 
 displayDaily();
